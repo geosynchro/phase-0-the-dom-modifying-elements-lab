54ae642079c3cb5945788c5eef3c9fa90423c897
Nicholas Fagner <nicholasfagner@gmail.com>
2021-10-02 13:17:01 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const element = document.getElementById("main")
+
+element.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.className = "victory"
+newHeader.id = "victory"
+
+newHeader.textContent = "NICK is the champion"
